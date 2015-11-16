a158ea4f0d50f5f5cf71ec11ba85c249dff10bfe
zfzlj <zhaofeizlj@163.com>
2015-11-16 14:32:50 +0800

    Initial Commit

---
+++ AFNtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNtest
+//
+//  Created by mac on 15/11/16.
+//  Copyright (c) 2015年 ZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNtest
+//
+//  Created by mac on 15/11/16.
+//  Copyright (c) 2015年 ZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
